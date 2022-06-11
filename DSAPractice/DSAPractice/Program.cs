3178235701fe8d7a5f2f4a45e9965cba2c4a80f0
Vivek Bajpai <VivekBajpai2013@gmail.com>
2022-06-12 04:56:46 +0530

    Day-1: Solve basic recursion and backtracking problems

---
+++ DSAPractice/DSAPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using DSAPractice.RecursionProblems;
+using System;
+
+namespace DSAPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print name five times
+            //PrintNameFiveTimes.PrintName("Vivek Bajpai");
+            //PrintNameFiveTimes.PrintOneToN(10);
+            //PrintNameFiveTimes.PrintNToOne(10);
+            PrintNameFiveTimes.PrintNtoOneBacktracking(1, 10);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DSAPractice/DSAPractice/RecursionProblems/PrintNameFiveTimes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPractice.RecursionProblems
+{
+    public static  class PrintNameFiveTimes
+    {
+        static int count = 5;
+        static string name;
+        static int no = 0;
+        public static void  PrintName(string Name)
+        {
+            name = Name;
+            PrintFiveTimes();
+        }
+
+        static void PrintFiveTimes()
+        {
+            if(count>0)
+            {
+                Console.WriteLine(name);
+                count--;
+                PrintFiveTimes();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public static void PrintOneToN(int n)
+        {
+            if (no<n)
+            {
+                no++;
+                Console.WriteLine(no);
+                PrintOneToN(n);
+                
+            }
+        }
+        public static void PrintNToOne(int n)
+        {
+            if (n>0)
+            {
+                Console.WriteLine(n);
+                n--;
+                PrintNToOne(n);
+
+            }
+        }
+
+        public static void PrintNtoOneBacktracking(int i, int n)
+        {
+            if (i > n)
+            {
+                return;
+            }
+
+            PrintNtoOneBacktracking(i + 1, n);
+            Console.WriteLine(i);
+        }
+
+
+    }
+}
